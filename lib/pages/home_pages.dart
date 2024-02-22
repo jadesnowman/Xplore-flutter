4c074d6e2718b345898427e5d3188e91b265f2e9
ilhamsj <ilhamsaputrajati@gmail.com>
2024-02-23 02:13:17 +0700

    Add theme and update home page UI

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,79 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:learning/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        backgroundColor: backgroundColor,
         body: SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              'Profile Picture',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Image(
-              image: AssetImage('assets/images/primary.png'),
-              height: 140,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text('Anne Margaritha',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-            SizedBox(
-              height: 4,
-            ),
-            Text('UX Designer',
-                style: TextStyle(fontSize: 16, color: Colors.grey)),
-            SizedBox(
-              height: 70,
-            ),
-            Wrap(
-              spacing: 38,
-              runSpacing: 40,
+          child: Center(
+            child: Column(
               children: [
-                Image(
-                  image: AssetImage('assets/images/item-1.png'),
-                  height: 80,
+                const SizedBox(
+                  height: 50,
                 ),
-                Image(
-                  image: AssetImage('assets/images/item-2.png'),
-                  height: 80,
+                const Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                Image(
-                  image: AssetImage('assets/images/item-3.png'),
-                  height: 80,
+                const SizedBox(
+                  height: 50,
                 ),
-                Image(
-                  image: AssetImage('assets/images/item-4.png'),
-                  height: 80,
+                const Image(
+                  image: AssetImage('assets/images/primary.png'),
+                  height: 140,
                 ),
-                Image(
-                  image: AssetImage('assets/images/item-5.png'),
-                  height: 80,
+                const SizedBox(
+                  height: 16,
                 ),
-                Image(
-                  image: AssetImage('assets/images/item-1.png'),
-                  height: 80,
+                const Text('Anne Margaritha',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text('UX Designer',
+                    style: TextStyle(fontSize: 16, color: Colors.grey)),
+                const SizedBox(
+                  height: 70,
+                ),
+                const Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/item-4.png'),
+                      height: 80,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/item-2.png'),
+                      height: 80,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/item-3.png'),
+                      height: 80,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/item-1.png'),
+                      height: 80,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/item-5.png'),
+                      height: 80,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/item-2.png'),
+                      height: 80,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                SizedBox(
+                  width: 224,
+                  height: 55,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: textColor,
+                    ),
+                    onPressed: () {
+                      print('Button pressed');
+                    },
+                    child: const Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
                 ),
               ],
-            )
-          ],
-        ),
-      ),
-    ));
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFFE5E5E5);
+const textColor = Color(0xFF314728);
+
+const primaryColor = Colors.blue;
+const secondaryColor = Color(0x00a9b0a6);
+const defaultPadding = 20.0;
